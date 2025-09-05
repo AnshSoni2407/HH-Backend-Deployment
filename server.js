225318a3bb976b3f7adbcc3a85805bd1966b717b
Ansh soni verma <anshsoni2407@gmail.com>
2025-09-05 15:48:59 +0530

    cors added

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//  Allowed origins
+// Allowed origins (local + deployed frontend)
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
-  "https://hh-frontend-Deployment.vercel.app",
+  "https://hh-frontend-deployment.vercel.app",
 ];
 
-//  CORS setup
+// CORS setup
 app.use(
   cors({
     origin: function (origin, callback) {
@@ -29,21 +29,21 @@ app.use(
         callback(new Error("Not allowed by CORS"));
       }
     },
-    credentials: true, // cookies / JWT ke liye zaroori
+    credentials: true,
   })
 );
 
-//  Middlewares
+// Middlewares
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
-//  Routes
+// Routes
 app.use("/auth", authRoutes);
 app.use("/job", jobRoutes);
 app.use("/application", applicationRoutes);
 
-//  Start Server + DB connect
+// Start Server + DB connect
 app.listen(PORT, () => {
   console.log("Server is running on port =", PORT);
   connectDB();
