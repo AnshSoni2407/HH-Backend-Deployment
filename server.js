98071ccda993a1945deb8a3044cd8f59fb0a6ed7
Ansh soni verma <anshsoni2407@gmail.com>
2025-09-06 02:33:59 +0530

    git

---
+++ server.js
@@ -0,0 +1,49 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./DB/db.connection.js";
+import cookieParser from "cookie-parser";
+import authRoutes from "./Routes/Auth.routes.js";
+import jobRoutes from "./Routes/Job.routes.js";
+import applicationRoutes from "./Routes/Application.routes.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Allowed origins (local + deployed frontend)
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "https://hh-frontend-deployment.vercel.app", // ✅ deploy wali link bilkul same honi chahiye (lowercase)
+];
+
+// ✅ CORS setup
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true, // ✅ isse cookie/jwt allow hoga
+  })
+);
+
+// ✅ Middlewares
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+
+// ✅ Routes
+app.use("/auth", authRoutes);
+app.use("/job", jobRoutes);
+app.use("/application", applicationRoutes);
+
+// ✅ Start Server + DB connect
+app.listen(PORT, () => {
+  console.log("Server is running on port =", PORT);
+  connectDB();
+});
+
+// For testing purpose
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
